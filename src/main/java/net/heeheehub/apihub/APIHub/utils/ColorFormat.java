843e9dca1d01f1fba863024e9a7ea5d5ae0a78e9
naphon <thewhalezaza900@gmail.com>
2025-08-09 09:44:04 +0700

    Commit

--- src/main/java/net/heeheehub/apihub/APIHub/utils/ColorFormat.java
+++ src/main/java/net/heeheehub/apihub/APIHub/utils/ColorFormat.java
@@ -2,10 +2,28 @@ package net.heeheehub.apihub.APIHub.utils;
 
 import org.bukkit.ChatColor;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
 public class ColorFormat {
 
-    public static String format(String str){
-        return ChatColor.translateAlternateColorCodes('&', str);
+	private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+    private static final LegacyComponentSerializer LEGACY_AMP = LegacyComponentSerializer.legacyAmpersand();
+	
+    public static String format(String input) {
+    	Component component = LegacyComponentSerializer.legacyAmpersand().deserialize(input);
+        return LegacyComponentSerializer.legacySection().serialize(component);
     }
-
+    
+    public static Component format2(String str){
+        Component component = LEGACY_AMP.deserialize(str);
+        
+        String miniMsgFormat = MINI_MESSAGE.serialize(component);
+        
+        Component c2 = MINI_MESSAGE.deserialize(miniMsgFormat);
+        
+        return c2;
+        
+    }
 }
